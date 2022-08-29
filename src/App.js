d317b177a467f1c22e020bf39b6aa7b16df53cd6
ibrammalik <malikmaul1173@gmail.com>
2022-08-29 14:05:43 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="quote-box">
+          <p id="text">lorem ipsum sir dolor ametttttttttttttttttt</p>
+          <p id="author">- maulana malik ibrahim</p>
+          <div className="group-button">
+            <button id="tweet-quote" target="_blank">
+              Tweet Quote
+            </button>
+            <button id="new-quote">New Quote</button>
+          </div>
+        </div>
       </header>
     </div>
   );
