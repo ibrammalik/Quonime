a416a11a36381bf20ae6f2a47bc9b6639e4e3593
ibrammalik <malikmaul1173@gmail.com>
2022-08-29 15:24:00 +0700

    Simple Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import $ from "jquery";
 
 function App() {
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+
+  const updateQuote = () => {
+    $.ajax({
+      url: "https://api.quotable.io/random",
+      success: (data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      },
+    });
+  };
+
+  const href = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
+
+  useEffect(() => {
+    updateQuote();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <div id="quote-box">
-          <p id="text">lorem ipsum sir dolor ametttttttttttttttttt</p>
-          <p id="author">- maulana malik ibrahim</p>
+          <p id="text">{quote}</p>
+          <p id="author">- {author}</p>
           <div className="group-button">
-            <button id="tweet-quote" target="_blank">
-              Tweet Quote
+            <a target="_top" href={href}>
+              <button id="tweet-quote">Tweet Quote</button>
+            </a>
+            <button id="new-quote" onClick={updateQuote}>
+              New Quote
             </button>
-            <button id="new-quote">New Quote</button>
           </div>
         </div>
       </header>
