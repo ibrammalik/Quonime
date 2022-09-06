8d5c18f6cf299ff5d2bdcae94f8c0deaa6d8cbd9
ibrammalik <malikmaul1173@gmail.com>
2022-09-06 17:24:21 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import $ from "jquery";
+import Footer from "./component/Footer";
 
 function App() {
   const [quote, setQuote] = useState();
   const [author, setAuthor] = useState();
-
-  const updateQuote = () => {
-    $.ajax({
-      url: "https://api.quotable.io/random",
-      success: (data) => {
-        setQuote(data.content);
-        setAuthor(data.author);
-      },
-    });
-  };
-
-  const href = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
+  const [anime, setAnime] = useState();
 
   useEffect(() => {
     updateQuote();
   }, []);
 
+  const updateQuote = () => {
+    console.log("ok");
+    // $.ajax({
+    //   url: "https://animechan.vercel.app/api/random",
+    //   success: (data) => {
+    //     setQuote(data.quote);
+    //     setAuthor(data.character);
+    //     setAnime(data.anime);
+    //   },
+    // });
+  };
+
+  const href = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div id="quote-box">
+        <div id="quote-box" className="shadow">
+          <p id="anime">{anime}</p>
           <p id="text">{quote}</p>
           <p id="author">- {author}</p>
           <div className="group-button">
-            <a target="_top" href={href}>
-              <button id="tweet-quote">Tweet Quote</button>
+            <a target="_top" href={href} id="tweet-quote">
+              <button className="button">Tweet Quote</button>
             </a>
-            <button id="new-quote" onClick={updateQuote}>
+            <button id="new-quote" onClick={updateQuote} className="button">
               New Quote
             </button>
           </div>
         </div>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <a href="https://www.flaticon.com/authors/freepik" title="q icons" target="_blank" rel="noreferrer">
+        Q icons created by Freepik - Flaticon
+      </a>
+      <a href="https://github.com/rocktimsaikia/anime-chan" target="_blank" rel="noreferrer">
+        Animechan API created by rocktimsaikia - Github
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
