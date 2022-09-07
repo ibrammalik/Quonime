d945f3e249de8ae726b5c04af9683e87f616c534
ibrammalik <malikmaul1173@gmail.com>
2022-09-07 09:48:07 +0700

    Update 2.0.0
    + Add Dark Mode Feature
    
    + Update Interactive UI

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,39 +7,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <footer id="footer">
+    <footer id="footer" className="color-white">
       <div id="footer-content">
         <div id="logo">
           <img id="website-logo" src={Logo} alt="Website Logo Quonime Q"></img>
-          <p id="website-name" className="margin-0">
+          <p id="website-name" className="margin-0 title-text">
             Quonime
           </p>
         </div>
         <div id="credit-container">
-          <p id="credit-title" className="margin-0">
+          <p id="credit-title" className="margin-0 title-text">
             Credit
           </p>
-          <a href="https://www.flaticon.com/authors/freepik" title="q icons" target="_blank" rel="noreferrer" className="t-decor-none color-white">
-            Q icons created by Freepik - Flaticon
-          </a>
-          <a href="https://github.com/rocktimsaikia/anime-chan" target="_blank" rel="noreferrer" className="t-decor-none color-white">
-            Animechan API created by rocktimsaikia - Github
-          </a>
+          <ul>
+            <li>
+              <a href="https://www.flaticon.com/authors/freepik" title="q icons" target="_blank" rel="noreferrer" className="t-decor-none color-white link">
+                Q icons created by Freepik - Flaticon
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/rocktimsaikia/anime-chan" target="_blank" rel="noreferrer" className="t-decor-none color-white link">
+                Animechan API created by rocktimsaikia - Github
+              </a>
+            </li>
+          </ul>
         </div>
         <div id="aboutme" className="left-align">
-          <p id="aboutme-title" className="margin-0">
+          <p id="aboutme-title" className="margin-0 title-text">
             About Me
           </p>
           <div id="aboutme-detail" className="d-flex">
             <img src={Profile} id="profile-picture" alt="profile-ibrammalik"></img>
             <div id="social-media-container">
-              <p id="social-media-title" className="margin-0">
+              <p id="social-media-title" className="margin-0 title-text">
                 Social Media
               </p>
-              <a href="https://github.com/ibrammalik" target="_blank" rel="noreferrer" className="d-block t-decor-none color-white">
+              <a href="https://github.com/ibrammalik" target="_blank" rel="noreferrer" className="d-block t-decor-none color-white link">
                 <FontAwesomeIcon icon={faGithub} /> <span>ibrammalik</span>
               </a>
-              <a href="https://instagram.com/ibra_mmalik" target="_blank" rel="noreferrer" className="d-block t-decor-none color-white">
+              <a href="https://instagram.com/ibra_mmalik" target="_blank" rel="noreferrer" className="d-block t-decor-none color-white link">
                 <FontAwesomeIcon icon={faInstagram} /> <span>ibra_mmalik</span>
               </a>
             </div>
--- src/component/QuoteBox.js
+++ src/component/QuoteBox.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import $ from "jquery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function QuoteBox() {
   const [quote, setQuote] = useState();
   const [author, setAuthor] = useState();
   const [anime, setAnime] = useState();
+  const [modeIcon, setModeIcon] = useState(faSun);
+  const [mode, setMode] = useState();
 
   const updateQuote = () => {
     $.ajax({
@@ -21,18 +25,48 @@ function QuoteBox() {
     updateQuote();
   }, []);
 
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme:dark)").matches;
+    if (localStorage.getItem("mode") === null) {
+      prefersDarkMode ? localStorage.setItem("mode", "dark") : localStorage.setItem("mode", "light");
+    }
+    setMode(localStorage.getItem("mode"));
+  }, []);
+
+  useEffect(() => {
+    if (mode === "light" && document.getElementById("root").classList.contains("darktheme")) {
+      document.getElementById("root").classList.remove("darktheme");
+      setModeIcon(faSun);
+    } else if (mode === "dark") {
+      document.getElementById("root").classList.add("darktheme");
+      setModeIcon(faMoon);
+    }
+  }, [mode]);
+
+  const modeToggleClickHandler = () => {
+    if (mode === "dark") {
+      localStorage.setItem("mode", "light");
+    } else if (mode === "light") {
+      localStorage.setItem("mode", "dark");
+    }
+    setMode(localStorage.getItem("mode"));
+  };
+
   const href = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
 
   return (
     <div id="quote-box" className="shadow">
-      <p id="anime">{anime}</p>
+      <div id="quote-box-header">
+        <p id="anime">{anime}</p>
+        <FontAwesomeIcon icon={modeIcon} id="mode-toggle" onClick={modeToggleClickHandler} />
+      </div>
       <p id="text">{quote}</p>
       <p id="author">- {author}</p>
       <div className="group-button">
         <a target="_top" href={href} id="tweet-quote">
-          <button className="button">Tweet Quote</button>
+          <button className="button color-white">Tweet Quote</button>
         </a>
-        <button id="new-quote" onClick={updateQuote} className="button">
+        <button id="new-quote" onClick={updateQuote} className="button color-white">
           New Quote
         </button>
       </div>
